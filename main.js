586c72e13780d1f5f2c1ad91995b15e826d183af
Zhiheng Yi <120039015@qq.com>
2014-11-18 12:49:13 -0500

    Finished the buttons of all natural deduction rules

--- main.js
+++ main.js
@@ -1,2 +1,12 @@
+window.addEventListener("load", init);
 function init () {
+	setListener();
+};
+
+function setListener(){
+	var conclude_button = document.getElementById("conclude");
+	var premise_button = document.getElementById("premise");
+	var assumption_button = document.getElementById("assumption");
+	var clear_button = document.getElementById("clear");
+	var undo_button = document.getElementById("undo");
 };
